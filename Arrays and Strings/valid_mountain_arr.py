2f0065699cd9572825a07fce7688df7edd5721de
ada0110 <64487038+ada0110@users.noreply.github.com>
2021-05-30 01:01:44 +0530

    Update valid_mountain_arr.py

--- Arrays and Strings/valid_mountain_arr.py
+++ Arrays and Strings/valid_mountain_arr.py
@@ -16,17 +16,25 @@ from typing import List
 
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
+        if (len(arr)<3):
+            return False
+        
         i = 1
+        # checking for increasing subarray on left
         while (i<len(arr) and arr[i] > arr[i-1]):
             i += 1
+        # if no increasing or no decreasing sequence in array    
         if (i==1 or i==len(arr)):
             return False
+         # checking for decreasing subarray on right
         while (i<len(arr) and arr[i] < arr[i-1]):
             i += 1
-        
+         # if we obtain a mountain then return true
         return (i == len(arr))
 
-
+# creating instance of a class
 s = Solution()
+
+# calling the method with given array 
 ans = s.validMountainArray([0,2,3,4,5,2,1])
 print(ans)
