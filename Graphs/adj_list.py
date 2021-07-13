0f29896ea35c35764270ad73f6247507799f09ca
ada0110 <64487038+ada0110@users.noreply.github.com>
2021-07-13 11:24:39 +0530

    undirected graph using adj list

---
+++ Graphs/adj_list.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    
+    def addEdge(self, v1, v2):
+        self.graph[v1].append(v2)
+        # print(self.graph)
+
+    def printGraph(self):
+        for node in self.graph:
+            values = self.graph[node]
+            print(node, "==>" , values)
+            # for val in self.graph[node]:
+                # print("val:", val)
+            #     print(node, "==>", val)
+
+
+    
+
+
+
+g = Graph()
+
+g.addEdge(1, 2)
+g.addEdge(1,3)
+g.addEdge(3, 4)
+g.addEdge(4,2)
+
+
+g.printGraph()
