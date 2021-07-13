69f52463ebd30ebfc1d408cf2b9a0a2cb457392d
ada0110 <64487038+ada0110@users.noreply.github.com>
2021-07-13 09:40:25 +0530

    max sum using prefix

---
+++ Recursion/interview.py
@@ -0,0 +1,26 @@
+
+
+
+def func(arr, list_tuples):
+    t = len(list_tuples)
+    print("t:", t)
+    print('\n')
+
+    for elem in list_tuples:
+        print(elem)
+        l = elem[0]
+        r = elem[1]
+        print(f"l:{l}, r:{r}")
+
+        for i in range(l,r+1):
+            arr[i] = arr[i] + 100
+        print(arr)
+        print('\n')
+
+    print("max elem:", max(arr))
+    
+
+func([10,10,10,10,10], [(1,3), (0,2)])
+
+
+
