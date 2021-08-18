3d073d59f94a44c1744941dc0de974eaafe84238
ada0110 <64487038+ada0110@users.noreply.github.com>
2021-08-18 22:43:32 +0530

    updated house robbers

--- Dynamic Programming/#198_house_robber.py
+++ Dynamic Programming/#198_house_robber.py
@@ -6,10 +6,6 @@ class Solution:
             return 0
         dp = [0] * n
         dp[0] = nums[0]
-        # dp[1] = max(nums[0], nums[1])
-        # for i in range(2, len(nums)):
-        #     dp[i] = max(dp[i-1], nums[i]+dp[i-2])
-        # can be modified as
         for i in range(1, len(nums)):
             if i==1:
                 dp[i] = max(nums[0], nums[1])
@@ -17,9 +13,9 @@ class Solution:
                 dp[i] = max(dp[i-1], dp[i-2]+nums[i])
         print(dp.index(dp[-1]))
         return dp[-1]
-        
+
 
 
 s = Solution()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
 ans = s.rob([1,2,3,1])#[2,7,9,3,1])#
-print(ans)
+# print(ans)
